678e553dabcd86e6e84c66520fa04aebb6685017
Mithrandiirr <swimox17@protonmail.com>
2023-05-05 21:24:37 +0100

    adding source code link

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Header } from "./Header";
+import Link from "next/link";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,8 +11,14 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="max-w-[1920px] mx-auto">
       <Header />
       <div className="px-28 py-8 md:px-16 mdx:px-8">{children}</div>
-      <footer className="flex flex-r justify-center items-center py-16  ">
+      <footer className="flex flex-row justify-center items-center py-16 gap-8  ">
         @Ensa fes/2022-2023
+        <Link
+          className="underline"
+          href={"https://github.com/Mithrandiirr/gtr1"}
+        >
+          Source code
+        </Link>
       </footer>
     </div>
   );
